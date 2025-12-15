0c9b7c6747dfc48a94ab124732a2e4f0b7fb51fc
dnte.jauregui <dnte.jauregui@gmail.com>
2025-12-15 07:55:21 +0000

    adding folders for different sources for python-tasks

---
+++ abhishek-python-challenges/Day-06/02-Assignment/01-Questions/task01.py
@@ -0,0 +1,15 @@
+total = 10
+# += is an assignment operator which is a short form.
+# In the below case it is equivalent to:
+# total = total + 5
+# total += 5
+
+total -= 2
+total *= 3
+total /= 2
+print(total)
+
+
+
+
+
